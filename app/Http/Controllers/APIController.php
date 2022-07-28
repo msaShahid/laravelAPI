f32e2fb258d731756ba4cbb9638be30f095d81be
Mohammad Shahid <msa.ansari28@gmail.com>
2022-07-28 13:40:21 +0530

    In this i have done 1. GET API and make Controller, Mehtod/function 2. In api.php i have make Route for Api

---
+++ app/Http/Controllers/APIController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class APIController extends Controller
+{
+    public function getUsers($id=null){
+        if(empty($id)){
+            // if ID is empty then this will run and Return All Data
+            $users = User::get();
+            //  return $users;
+            return response()->json(["users"=>$users]); 
+        }else{
+            // if ID found then this will run and Return Single Data 
+            $users = User::find($id);
+            return response()->json(["user"=>$users]);
+        }
+    }
+}
